4629da0dc5f06829cd0939d53464e4fd60e027ed
sizm0919 <shunsuke.izumi@subak-tech.com>
2022-04-12 15:24:25 +0900

    master

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,19 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-const { environment } = require('@rails/webpacker')
-
-// jQueryとBootstapのJSを使えるようにする
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
-// ここまで
-
-module.exports = environment
